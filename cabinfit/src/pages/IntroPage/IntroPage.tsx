f32f541d786a7fc1438452688eb32aaef26720e6
Ahmet Enes Çırak <enes.crk@gmail.com>
2022-04-17 11:41:17 +0300

    Added | IntroPage and Its styles

--- cabinfit/src/pages/IntroPage/IntroPage.tsx
+++ cabinfit/src/pages/IntroPage/IntroPage.tsx
@@ -18,15 +18,13 @@ export default function IntroPage() {
                 <img src="https://www.yatasbedding.com.tr/Themes/Yatas/Content/survey/static/media/home-sleep2.png" alt="intro image" />
 
                 <p>
-                    Bilinçsiz ve deneme yanılma yöntemleriyle yapılan yatak seçimini engellemek amacıyla, Yataş Uyku Laboratuvarı tarafından hazırlanan DOĞRU YATAK BULMA SİSTEMİ SMART MATCH'E giriş yapmak üzeresiniz.
-
-                    Yataş yataklarının %97,8 oranındaki müşteri memnuniyetini %100’e yükseltmeyi amaçlayan Smart Match sistemi; ilerleyen adımlarda bizimle paylaşacağınız size özel bilgileri ve uyku ihtiyaçlarınızı yapılan araştırma ve testler sonucunda belirlenen algoritmalar ile değerlendirerek sizin için en doğru yatağı önerir.
+                    CabinFit ile kendi vücut ölçüleriniz ve tercihlerinize göre size en uygun bedeni bulmak üzeresin. Mağazadaki kabin deneyimini online ortamda CabinFit ile yaşayabilirsin. %95 doğruluk oranı ile birkaç pratik soru ile seni daha iyi tanımak istiyoruz. Hazırsan...
                 </p>
 
                 <div className="customBtn">
-                <CustomButton  text="Hemen Başlayalım" clickedOnBtn={clickedOnBtn} disabled={false} />
+                    <CustomButton text="Hemen Başlayalım" clickedOnBtn={clickedOnBtn} disabled={false} />
                 </div>
-                
+
             </section>
         </div>
     )
